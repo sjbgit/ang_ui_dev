9f517d457f525cd2a7ba707be59d049ca0297093
sjbgit <sjb_strat@hotmail.com>
2015-01-28 17:14:41 -0600

    before grunt changes

---
+++ karma.conf.js
@@ -0,0 +1,19 @@
+/**
+ * Created by sbunke on 1/28/2015.
+ */
+module.exports = function(config){
+    config.set({
+        basePath : '../',
+        files : [
+            'src/bower/angular/angular.js',
+            'src/bower/angular-mocks/angular-mocks.js',
+            'src/js/**/*.js',
+            'test/unit/**/*.js'
+        ],
+        autoWatch: true,
+        frameworks: ['jasmine'],
+        browsers : ['Chrome', 'Firefox'],
+        plugins : ['karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-jasmine']
+    })};
